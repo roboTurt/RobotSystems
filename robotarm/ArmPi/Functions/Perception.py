2618897732d17c0a5eada7b6a467bf6f407d31a3
Fangzhou Yu <yufangzh@oregonstate.edu>
2022-02-21 19:22:47 -0800

    fixes to rossros integration

--- robotarm/ArmPi/Functions/Perception.py
+++ robotarm/ArmPi/Functions/Perception.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.getcwd()))
 
 import cv2
 import math
-from ArmPi.CameraCalibration import *
+from CameraCalibration import *
 from LABConfig import *
 from ArmIK.Transform import *
 import HiwonderSDK.Board as Board
